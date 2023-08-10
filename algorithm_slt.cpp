8b5db21bbb56e4d280c4258efc442665883b9b4b
SKYjithyapa <125755221+SKYjithyapa@users.noreply.github.com>
2023-08-10 08:34:08 +0530

    first commit

---
+++ algorithm_slt.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+int i ;
+cin>> i;
+int j = 10;
+
+cout << "Value of i " << i << endl;
+cout << "Value of j " << j << endl;
+
+swap(i,j);
+
+cout << "After swaping i "<< i << endl;
+cout << "After swaping j "<< j << endl;
+
+
+
+
+
+return 0;
+}
